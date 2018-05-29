567600840f6534441aeb9bb5377cacb57c4c039c
shin <shinchen03@gmail.com>
2018-05-29 03:14:03 -0500

    jump
    jump by touch

--- app/src/main/java/com/example/shin/action_game/GameView.java
+++ app/src/main/java/com/example/shin/action_game/GameView.java
@@ -5,12 +5,42 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class GameView extends View implements Droid.Callback {
     private static final int START_GROUND_HEIGHT = 50;
     private Ground ground;
     private Droid droid;
+    private static final int MAX_TOUCH_TIME = 500;
+    private long touchDownStartTime;
+    @Override
+    public boolean onTouchEvent (MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                touchDownStartTime = System.currentTimeMillis();
+                return true;
+            case MotionEvent.ACTION_UP:
+                jumpDroid();
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    private void jumpDroid(float power) {
+        float time = System.currentTimeMillis() - touchDownStartTime;
+        touchDownStartTime = 0;
+
+        if (getDistanceFromGround(droid) != 0) {
+            return;
+        }
+
+        if (time > MAX_TOUCH_TIME) {
+            time = MAX_TOUCH_TIME;
+        }
+
+        droid.jump(time / MAX_TOUCH_TIME);
+    }
 
     public GameView(Context context) {
         super(context);
@@ -43,5 +73,5 @@ public class GameView extends View implements Droid.Callback {
     @Override
     public int getDistanceFromGround(Droid droid) {
         return ground.rect.top - droid.rect.bottom;
-     }
+    }
 }
